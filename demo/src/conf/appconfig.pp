721be4f4ec354a5141b1d516f8eafd8389c9151a
Siamak Sadeghianfar <ssadeghi@redhat.com>
2014-08-11 16:56:29 +0200

    puppet config added

---
+++ demo/src/conf/appconfig.pp
@@ -0,0 +1,21 @@
+#####################################
+#                                   #
+#  Ticket Monster Puppet Config     #
+#                                   #
+#####################################
+
+# Stateless Session Beans Max Pool Size
+$slsb_max_pool_size = 30
+
+exec {'stateless session beans pool size':
+  command =>  "/bin/sed -i \"s/name=\\\"slsb-strict-max-pool\\\" max-pool-size=\\\"20\\\"/name=\\\"slsb-strict-max-pool\\\" max-pool-size=\\\"$slsb_max_pool_size\\\"/g\" /home/jbosseap/eap/jboss-eap-6.3/standalone/configuration/standalone.xml"
+}
+
+# 
+exec {'soft limits':
+  command =>  "/bin/echo \"* soft nofile 4096\" >> /etc/security/limits.conf"
+}
+
+exec {'hard limits':
+  command =>  "/bin/echo \"* hard nofile 4096\" >> /etc/security/limits.conf"
+}
